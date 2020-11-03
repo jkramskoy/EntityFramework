02392f4fbf045944cd07813e0d493d9980c19ae9
JuliaKramskoy <julia.kramskoy@gmail.com>
2020-11-02 20:47:54 -0600

    add ef from scaffold using CLI command

---
+++ EntityFramework/EntityFramework/Entity_FrameworkContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EntityFramework.EntityFramework
+{
+    public partial class Entity_FrameworkContext : DbContext
+    {
+        public Entity_FrameworkContext()
+        {
+        }
+
+        public Entity_FrameworkContext(DbContextOptions<Entity_FrameworkContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Departments> Departments { get; set; }
+        public virtual DbSet<Employees> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=amy251202;database=Entity_Framework");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Departments>(entity =>
+            {
+                entity.ToTable("Departments", "Entity_Framework");
+
+                entity.Property(e => e.Location).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Employees>(entity =>
+            {
+                entity.ToTable("Employees", "Entity_Framework");
+
+                entity.HasIndex(e => e.DepartmentId)
+                    .HasName("DepartmentId");
+
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+
+                entity.Property(e => e.LastName).HasMaxLength(50);
+
+                entity.HasOne(d => d.Department)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.DepartmentId)
+                    .HasConstraintName("employees_ibfk_1");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
