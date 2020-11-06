96dde764760c0d0f10affa1b2609977b098451bc
JuliaKramskoy <julia.kramskoy@gmail.com>
2020-11-05 19:40:53 -0600

    weird2

--- Northwind/NorthwindData/Categories.cs
+++ Northwind/NorthwindData/Categories.cs
@@ -10,11 +10,13 @@ namespace Northwind.NorthwindData
             Products = new HashSet<Products>();
         }
 
+        // these properties map to columns in the database
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
         public string Description { get; set; }
         public byte[] Picture { get; set; }
 
+        // defines a navigation property for related rows 
         public virtual ICollection<Products> Products { get; set; }
     }
 }
